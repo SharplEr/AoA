52b7eb599a3741f6b5131e480ff66a3c755d1423
SharplEr <SharplEr@gmail.com>
2014-11-02 14:16:00 +0300

    Up annealing
    Signed-off-by: SharplEr <SharplEr@gmail.com>

--- Metaheuristics/Metaheuristics/AnnealingNeighborhood.cs
+++ Metaheuristics/Metaheuristics/AnnealingNeighborhood.cs
@@ -25,16 +25,20 @@ namespace Metaheuristics
 
             for(int i = 0; i< ans.Length; i++)
             {
-                double z = random.NextDouble();
-                double t = temp[i]();
-                z = Math.Sign(z - 0.5) * t * (Math.Pow(1.0 + 1.0 / t, Math.Abs(2 * z - 1)) - 1);
-                
-                //Если требуется найти t, по известному z, то t=-z*z/(2*z-1), при z0 = 0.75.
-                ans[i] = x[i] + (int)Math.Round(z * (parameters[i].length));
+                ans[i] = x[i];
+                if (parameters[i].length != 0)
+                {
+                    double z = random.NextDouble();
+                    double t = temp[i]();
+                    z = Math.Sign(z - 0.5) * t * (Math.Pow(1.0 + 1.0 / t, Math.Abs(2 * z - 1)) - 1);
 
-                if (ans[i] > parameters[i].max) ans[i] = parameters[i].max;
-                else if (ans[i] < parameters[i].min) ans[i] = parameters[i].min;
-                
+                    //Если требуется найти t, по известному z, то t=-z*z/(2*z-1), при z0 = 0.75.
+                    ans[i] = (int)Math.Round(z * (parameters[i].length));
+
+                    if (ans[i] > parameters[i].max) ans[i] = parameters[i].max;
+                    else if (ans[i] < parameters[i].min) ans[i] = parameters[i].min;
+                }
+
                 Console.WriteLine("прыг: {0}", Math.Abs(ans[i] - x[i]));
             }
 
