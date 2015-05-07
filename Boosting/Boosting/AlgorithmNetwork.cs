b15ba1e1cb48c3e88354c46b505decbf584b650f
SharplEr <SharplEr@gmail.com>
2015-05-07 10:45:46 +0300

    Auto cleanup...
    Signed-off-by: SharplEr <SharplEr@gmail.com>

--- Boosting/Boosting/AlgorithmNetwork.cs
+++ Boosting/Boosting/AlgorithmNetwork.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AoA;
-using VectorSpace;
-
-namespace Boosting
+﻿namespace Boosting
 {
     /*
     public class AlgorithmNetwork
